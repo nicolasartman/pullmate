4adf36798564cce406ec7ecfd70bc03502791945
Nicolas Artman <nicolasartman@users.noreply.github.com>
2014-08-06 23:34:23 -0700

    Wrap in IIFE

--- source.js
+++ source.js
@@ -1,9 +1,11 @@
-$("tr.file-diff-line > td.diff-line-num-addition.empty-cell").each(function (index, cell) {
-  var line = $(cell).parent().attr('data-line');
-  var path = $(cell).parent().attr('data-path');
-  
-  // Fill in the path to the repo on your computer here
-  var repoRoot = '/Users/nick/reporoot/';
-  var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
-  $(cell).html('<a href="' + openInTextmateUrl + '">Txmt</a>');
-})
+(function (window, jQuery) {
+  $("tr.file-diff-line > td.diff-line-num-addition.empty-cell").each(function (index, cell) {
+    var line = $(cell).parent().attr('data-line');
+    var path = $(cell).parent().attr('data-path');
+    
+    // Fill in the path to the repo on your computer here
+    var repoRoot = '/Users/nick/reporoot/';
+    var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
+    $(cell).html('<a href="' + openInTextmateUrl + '">Txmt</a>');
+  })
+}(window, jQuery));
