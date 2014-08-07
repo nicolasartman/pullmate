d1b48b324e9e52c740121c76f78a6e495634febf
Nicolas Artman <nicolasartman@users.noreply.github.com>
2014-08-06 23:26:09 -0700

    Update to work with github's latest UI changes

--- source.js
+++ source.js
@@ -1,8 +1,8 @@
-$("tr.file-diff-line.gi").each(function (index, row) {
-  var line = $(row).attr('data-line');
-  var path = $(row).attr('data-path');
+$("tr.file-diff-line > td.diff-line-num-addition.empty-cell").each(function (index, cell) {
+  var line = $(cell).parent().attr('data-line');
+  var path = $(cell).parent().attr('data-path');
 	// Fill in the path to the repo on your computer here
 	var repoRoot = '/Users/nick/reporoot/';
 	var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
-  $(row).find('.line-num-content:eq(0)').html('<a href="' + openInTextmateUrl + '">Txmt</a>');
+  $(cell).html('<a href="' + openInTextmateUrl + '">Txmt</a>');
 })
