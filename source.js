b74597858d6557abe9fa8cc46d3453bae4649316
Nicolas Artman <nicolasartman@users.noreply.github.com>
2014-08-06 23:26:38 -0700

    Fix spacing

--- source.js
+++ source.js
@@ -1,8 +1,9 @@
 $("tr.file-diff-line > td.diff-line-num-addition.empty-cell").each(function (index, cell) {
   var line = $(cell).parent().attr('data-line');
   var path = $(cell).parent().attr('data-path');
-	// Fill in the path to the repo on your computer here
-	var repoRoot = '/Users/nick/reporoot/';
-	var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
+  
+  // Fill in the path to the repo on your computer here
+  var repoRoot = '/Users/nick/reporoot/';
+  var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
   $(cell).html('<a href="' + openInTextmateUrl + '">Txmt</a>');
 })
