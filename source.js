dbac88e132ccf8adb44a19ffaa26c142a86e6000
Nicolas Artman <nicolasartman@users.noreply.github.com>
2014-09-21 17:09:41 -0700

    Add support back for comment diff snippet view

--- source.js
+++ source.js
@@ -1,14 +1,27 @@
 (function (window, jQuery) {
   // Fill in the path to the repo on your computer here
   var repoRoot = '/Users/nick/reporoot/';
-
-  $('#files > .file').each(function (index, fileView) {
-    var path = $(fileView).children('[data-path]').eq(0).attr('data-path');
+  
+  // Grab all of the files in diff view
+  var fileViews = $('#files:visible > .file');
+  
+  // If we don't seem to be in diff view, grab all of the diff previews for comments in
+  // the main discussion view
+  if (!fileViews.length) {
+    fileViews = $('.file.inline-review-comment');
+  }
+  
+  if (fileViews.length) {
+    fileViews.each(function (index, fileView) {
+      var path = $(fileView).children('[data-path]').eq(0).attr('data-path');
     
-    $(fileView).find('[data-line-number]').off().click(function () {
-      var line = $(this).attr('data-line-number');
-      var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
-      window.open(openInTextmateUrl, '_self');
-    });
-  });
+      $(fileView).find('[data-line-number]').off().click(function () {
+        var line = $(this).attr('data-line-number');
+        var openInTextmateUrl = 'txmt://open?url=file://' + repoRoot + path + '&line=' + line;
+        window.open(openInTextmateUrl, '_self');
+      });
+    });    
+  } else {
+    window.alert('Failed to find any diff lines to linkify')
+  }
 }(window, jQuery));
